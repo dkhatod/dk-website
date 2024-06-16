f9e7322e200f1134ee8955790a50ac144fad05e8
dkhatod <dhruvkhatod@gmail.com>
2024-06-16 14:07:57 -0400

    Decided to upload my WIP.

---
+++ script.js
@@ -0,0 +1,21 @@
+//Baffle
+const text = baffle(".text__glitch");
+text.set({
+    characters: "█▓█ ▒░/▒░ █░▒▓/ █▒▒ ▓▒▓/█ ░█▒/ ▒▓░ █<░▒ ▓/░>",
+    speed: 120
+});
+text.start();
+text.reveal(4000);
+
+const cursor = document.querySelector('.cursor');
+
+document.addEventListener('mousemove', e => {
+    cursor.setAttribute("style", "top: " + (e.pageY - 10) + "px; left: " + (e.pageX - 10) + "px;")
+});
+
+document.addEventListener('click', e => {
+    cursor.classList.add("expand");
+    setTimeout(() => {
+        cursor.classList.remove("expand");
+    }, 500);
+});
